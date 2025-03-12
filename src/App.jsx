700f617d4cd459f60e0195bfd34d790883651080
KingShivamX <shivamhippalgave@gmail.com>
2025-03-12 21:52:23 +0530

    yo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 
 const App = () => {
-    return <div className="text-3xl font-bold underline"> HEllO Jiya BABY</div>
+    return (
+        <div className="text-3xl font-bold underline">
+            chakko ki koi kami nahi hai desh mein
+        </div>
+    )
 }
 
 export default App
